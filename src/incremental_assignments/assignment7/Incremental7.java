6a758c4b09419cb4af1296cb1f5dfcbca692f017
vineethk123 <vineeth.kolluru@zemosolabs.com>
2019-01-11 16:44:20 +0530

    add incremental assignment

---
+++ src/incremental_assignments/assignment7/Incremental7.java
@@ -0,0 +1,56 @@
+package incremental_assignments.assignment7;
+
+import javax.print.attribute.HashPrintJobAttributeSet;
+import java.io.*;
+import java.nio.charset.Charset;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+import java.util.Scanner;
+
+public class Incremental7 {
+
+    public static void main(String args[]) {
+        char ch;
+        String targetContent = "", targetFileName = args[0].replace(".txt", "_char_count.txt");
+        try{
+            // Creating Scanner instance to read from the specified file
+            Scanner scanner = new Scanner(new FileInputStream(args[0]));
+            scanner.useDelimiter("");    // Make scanner use empty string as delimiter
+            Map<Character, Integer> charCounts = new HashMap<Character, Integer>();
+
+            // Count the occurance of each character in the file
+            while (scanner.hasNext()) {
+                ch = scanner.next().charAt(0);
+                if (charCounts.containsKey(ch)) {
+                    charCounts.put(ch, charCounts.get(ch)+1);
+                }
+                else {
+                    charCounts.put(ch, 1);
+                }
+            }
+
+            // Get the iterator for charCounts object
+            Iterator iterator = charCounts.entrySet().iterator();
+
+            //File file = new File(args[0].replace(".txt", "_char_count.txt");
+            //if (file.createNewFile()) {
+                // Write the counts of characters to a file
+                while (iterator.hasNext()) {
+                    Map.Entry mapEntry = (Map.Entry) iterator.next();
+                    targetContent += mapEntry.getKey() + ", " + mapEntry.getValue() + "\n";
+                }
+            Files.write(Paths.get(targetFileName), targetContent.getBytes());
+            //}
+        }
+        catch(FileNotFoundException e) {
+            System.out.println("File doesn't exist. Please check the file name and try again.");
+        }
+        catch(IOException e) {
+            System.out.println("Error creating/writing the target file.");
+        }
+    }
+}
